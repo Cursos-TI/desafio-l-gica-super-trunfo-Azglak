5f17c08620a7335bed8a93671135b6c9db083afa
Azglak <gmps023@gmail.com>
2025-06-13 22:04:51 -0300

    Intermediário
    Nível intermediário adicionado.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -9,10 +9,12 @@ struct Cidade {
     char Nome[];
 };
 
+int AtributoChecado = 0;
+
 int main() {
     struct Cidade cidade1;
     struct Cidade cidade2;
-    
+
     printf("Olá, bem-vindo ao Super Trunfo Cidades!\nPor favor, insira os dados da primeira cidade.\n");
     printf("Nome:\n");
     scanf(" %s", &cidade1.Nome);
@@ -42,16 +44,89 @@ int main() {
     scanf("%f", &cidade2.PIB);
     printf("Número de pontos turísticos:\n");
     scanf("%d", &cidade2.NPTuristicos);
-    
+
     printf("%s é o lar de %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos, além de possuir %f de densidade demográfica e %f de PIB per capta!\n", cidade1.Nome, cidade1.População, cidade1.Area, cidade1.PIB, cidade1.NPTuristicos, cidade1.População/cidade1.Area, cidade1.PIB/cidade1.População);
-    
+
     printf("Já %s é populada por %d pessoas, tem %f de área, %f de PIB e possui %d pontos turísticos, além de possuir %f de densidade demográfica e %f de PIB per capta!\n\n",cidade2.Nome, cidade2.População, cidade2.Area, cidade2.PIB, cidade2.NPTuristicos, cidade2.População/cidade2.Area, cidade2.PIB/cidade2.População);
-    
-    if(cidade1.PIB/cidade1.População > cidade2.PIB/cidade2.População){
-        printf("A cidade de %s tem o maior PIB per capta e, portanto, é a vencedora!", cidade1.Nome);
-    } else {
-        printf("A cidade de %s tem o maior PIB per capta e, portanto, é a vencedora!", cidade2.Nome);
+
+    while (AtributoChecado <= 0 || AtributoChecado > 6) {
+           printf("Qual o atributo que será usado para a comparação?\n\n    1: População\n    2: Área\n    3: PIB\n    4: Número de Pontos Turísticos\n    5: PIB per Capta\n    6: Densidade Populacional\n\n");
+           scanf("%d", &AtributoChecado);
     }
+
+    switch (AtributoChecado) {
+        case 1:
+            printf("Comparando População:\n\n");
+            if (cidade1.População > cidade2.População){
+                printf("%s é a vencedora!\n", cidade1.Nome);
+            } else if (cidade1.População < cidade2.População){
+                printf("%s é a vencedora!\n", cidade2.Nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 2:
+            printf("Comparando Área:\n\n");
+            if (cidade1.Area > cidade2.Area){
+                printf("%s é a vencedora!\n", cidade1.Nome);
+            } else if (cidade1.Area < cidade2.Area){
+                printf("%s é a vencedora!\n", cidade2.Nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 3:
+            printf("Comparando PIB:\n\n");
+            if (cidade1.PIB > cidade2.PIB){
+                printf("%s é a vencedora!\n", cidade1.Nome);
+            } else if (cidade1.PIB < cidade2.PIB){
+                printf("%s é a vencedora!\n", cidade2.Nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 4:
+            printf("Comparando Número de Pontos Turísticos:\n\n");
+            if (cidade1.NPTuristicos > cidade2.NPTuristicos){
+                printf("%s é a vencedora!\n", cidade1.Nome);
+            } else if (cidade1.NPTuristicos < cidade2.NPTuristicos){
+                printf("%s é a vencedora!\n", cidade2.Nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 5:
+            printf("Comparando PIB per Capta:\n\n");
+            if (cidade1.PIB / cidade1.População > cidade2.PIB / cidade2.População){
+                printf("%s é a vencedora!\n", cidade1.Nome);
+            } else if (cidade1.PIB / cidade1.População < cidade2.PIB / cidade2.População){
+                printf("%s é a vencedora!\n", cidade2.Nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        case 6:
+            printf("Comparando Densidade Populacional:\n\n");
+            if (cidade1.População / cidade1.Area < cidade2.População / cidade2.Area){
+                printf("%s é a vencedora!\n", cidade1.Nome);
+            } else if (cidade1.População / cidade1.Area > cidade2.População / cidade2.Area){
+                printf("%s é a vencedora!\n", cidade2.Nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+
+        }
+
+
+
+
 
     return 0;
 }
+
